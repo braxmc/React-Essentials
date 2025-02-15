8dea0e0a57fdb9d69a80f481aca2b00e4ef7a2cc
Braxton McClellan <thebraxmc@gmail.com>
2025-02-15 01:20:18 -0700

    Review: Outputting Dynamic Contnet

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,17 @@
+const reactDescriptions = ['Fundamental', 'Cruicial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stytlized atom" />
-      <p>Fundamental React concepts you will need for almost any app you are going to build!</p>
+      <h1>React Essentials</h1>
+      <p>{description} React concepts you will need for almost any app you are going to build!</p>
     </header>
   );
 }
