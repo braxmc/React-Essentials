17ba9a5c77e9b7a524f4a84d243bfe4291b30b96
Braxton McClellan <thebraxmc@gmail.com>
2025-02-15 00:46:00 -0700

    Review: Displaying custom components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stytlized atom" />
+      <p>Fundamental React concepts you will need for almost any app you are going to build!</p>
+    </header>
+  );
+}
+export default Header;
