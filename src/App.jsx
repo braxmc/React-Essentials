47a408d699298fb1602ccd8b71c6e0b70032d521
Braxton McClellan <thebraxmc@gmail.com>
2025-02-15 02:20:55 -0700

    Review: Making components reusable with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import Header from './Components/Header'
+import CoreConcept from './Components/CoreConcept';
+
+import componentsImg from './assets/components.png'
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={componentsImg}
+            />
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={componentsImg}
+            />
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={componentsImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
