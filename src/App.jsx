bcd59a0abb18984ad70763359b80ceb111daa056
Braxton McClellan <thebraxmc@gmail.com>
2025-02-15 21:36:21 -0700

    Conditional Content

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,33 @@ import CoreConcept from './Components/CoreConcept/CoreConcept.jsx';
 import TabButton from './Components/TabButton/TabButton.jsx';
 
 import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES } from './data.js';
 
 import { useState } from 'react';
 
 function App() {
 
-  let [ selectedTopic, setSelectedTopic ] = useState('Please click a button.');
+  let [ selectedTopic, setSelectedTopic ] = useState();
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
     setSelectedTopic(selectedButton)
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>    
+    );
   }
 
   return (
@@ -40,7 +57,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {tabContent} 
         </section>
       </main>
     </div>
